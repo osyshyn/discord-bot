3c65562fcd7c235ea88355ab902e0d17365c424c
osyshyn <129138229+osyshyn@users.noreply.github.com>
2025-07-21 14:42:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const {
   // Removed InteractionResponseFlags import to avoid confusion if not directly exported
 } = require("discord.js");
 
-console.log("TOKEN:", process.env.BOT_TOKEN);
+console.log("TOKEN:", process.ENV.BOT_TOKEN);
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
